899228383b404fa01739316712efccf1cdfc4ecc
giu-g <133990052+giu-g@users.noreply.github.com>
2023-10-26 09:15:53 -0300

    Add files via upload

---
+++ PrjGame/src/main/java/com/senai/giuliag/PrjGame/entities/Jogo.java
@@ -0,0 +1,47 @@
+package com.senai.giuliag.PrjGame.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Jogo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "plataform")
+	private String plataform;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPlataform() {
+		return plataform;
+	}
+
+	public void setPlataform(String plataform) {
+		this.plataform = plataform;
+	}
+}
---
+++ PrjGame/src/main/java/com/senai/giuliag/PrjGame/services/JogoService.java
@@ -0,0 +1,50 @@
+package com.senai.giuliag.PrjGame.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.giuliag.PrjGame.entities.Jogo;
+import com.senai.giuliag.PrjGame.repositories.JogoRepository;
+
+@Service
+public class JogoService {
+	private final JogoRepository jogoRepository;
+
+	@Autowired
+	public JogoService(JogoRepository jogoRepository) {
+		this.jogoRepository = jogoRepository;
+	}
+
+	public Jogo saveJogo(Jogo jogo) {
+		return jogoRepository.save(jogo);
+	}
+	
+	public List<Jogo> getAllJogos() {
+		return jogoRepository.findAll();
+	}
+
+	public Jogo getJogoById(Long id) {
+		return jogoRepository.findById(id).orElse(null);
+	}
+
+	public void deleteJogo(Long id) {
+		jogoRepository.deleteById(id);
+	}
+
+	// fazendo o update do jogo com o optional
+	public Jogo updateJogo(Long id, Jogo novoJogo) {
+		Optional<Jogo> jogoOptional = jogoRepository.findById(id);
+		if (jogoOptional.isPresent()) {
+			Jogo jogoExistente = jogoOptional.get();
+			jogoExistente.setName(novoJogo.getName());
+			jogoExistente.setPlataform(novoJogo.getPlataform());
+			return jogoRepository.save(jogoExistente);
+		} else {
+			return null;
+		}
+	}
+
+}
